a0320533d323cb2cc6545f125878f94e8a8cda3e
Vaibhav Pandey <vp633668@gmail.com>
2026-04-10 13:14:16 +0530

    Add files via upload

---
+++ FactorialReduce.java
@@ -0,0 +1,14 @@
+package Reduce;
+import java.util.stream.IntStream;
+
+public class FactorialReduce {
+	public static void main(String args[]) {
+		int n=5;
+		int factorial = IntStream.rangeClosed(1,n)
+				.reduce(1,(a,b)->a*b);
+		
+		System.out.println("Factorial="+factorial);
+	
+	}
+
+}
